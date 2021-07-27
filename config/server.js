309286be111ab8acdc799707f977d6152945893a
PabloNunezRuiz <pablo.nunez.ruiz@gmail.com>
2021-07-27 14:10:30 +0200

    First Upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36738a4015b19d788338c99dd1b5fc93'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ea2cf315-bb45-4580-80c1-5084681ad580'
+};
